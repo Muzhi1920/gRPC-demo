6f014a26685f03372de97eb3e833acd9eba002f0
Muzhi1920 <191651196@qq.com>
2022-05-21 09:20:47 +0000

    op

--- grpc/python/client.py
+++ grpc/python/client.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-
 import grpc
 from proto import rec_pb2,rec_pb2_grpc
 import time
 
 
-def feed_articles(stub):
+def get_videos(stub):
+    # create user info request
     user_request = rec_pb2.user_request()
     user_request.user_id = "123"
     user_request.age = 32
@@ -14,21 +13,17 @@ def feed_articles(stub):
     user_request.time_stamp = int(time.time() * 1000)
     user_request.platform = "android"
 
-    # stub call
+    # call stub service func
     res = stub.rec_sys(user_request)
-    print('res is {}'.format(res))
+    print('rec res is {}'.format(res))
     return res
 
 
 def run():
-    """
-    client rpc run
-    """
-    # connect rpc server
     with grpc.insecure_channel('127.0.0.1:8888') as channel:
-        # create rpc stub and request
         stub = rec_pb2_grpc.RecSystemStub(channel)
-        feed_articles(stub)
+        get_videos(stub)
+
 
 if __name__ == '__main__':
     run()
--- grpc/python/server.py
+++ grpc/python/server.py
@@ -1,53 +1,48 @@
-# -*- coding: utf-8 -*-
-
 from proto import rec_pb2,rec_pb2_grpc
 import grpc
 from concurrent.futures import ThreadPoolExecutor
 import time
 
 
-# define return class
 class RecSystemService(rec_pb2_grpc.RecSystemServicer):
 
     def rec_sys(self, request, context):
-        # request是调用的请求数据对象
-
+        # parse user request info
         user_id = request.user_id
         age = request.age
         video_nums = request.video_nums
         time_stamp = request.time_stamp
+        print("rpc log : uid is {}, age is {}, time_stamp is {}, ctx is {}".format(user_id, age, time_stamp, context))
 
-        print("rpc log : cur info is {}, ctx is {}".format(request, context))
+        # make mock rec res
         response = rec_pb2.VideoResponse()
         response.impression = 'impression'
         response.time_stamp = round(time.time() * 1000)
         recsys_res = []
         for i in range(video_nums):
             video = rec_pb2.Video()
-            video.meta.cover = 'cover param {}'.format(i + 1)
-            video.meta.title = 'title param {}'.format(i + 1)
-            video.meta.up = 'up param {}'.format(i + 1)
-            video.meta.tag = 'tag param {}'.format(i + 1)
-            video.video_id = i + 1
+            video.meta.cover = 'cover_{}'.format(i)
+            video.meta.title = 'title_{}'.format(i)
+            video.meta.up = 'uploader_{}'.format(i)
+            video.meta.tag = 'tag_{}'.format(i)
+            video.video_id = i * 1000
             recsys_res.append(video)
         response.recsys_res.extend(recsys_res)
-        ## 返回响应
+
         return response
 
 
-def serve():
+def run():
     server = grpc.server(ThreadPoolExecutor(max_workers=10))
-
     rec_pb2_grpc.add_RecSystemServicer_to_server(RecSystemService(), server)
-
     server.add_insecure_port('127.0.0.1:8888')
 
     server.start()
     print('start ok')
     while True:
-        print('running')
+        print('server is running...')
         time.sleep(10)
 
 
 if __name__ == '__main__':
-    serve()
+    run()
